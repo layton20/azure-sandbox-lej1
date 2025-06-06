09430fd45d7bd361342cdfb9b20c46c928ba6846
Johnny Le <contact.lejohnny@gmail.com>
2025-06-07 00:37:35 +0100

    Add BaseEntity, EFCore nuget

---
+++ src/Sandbox.API/Sandbox.API/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Sandbox.API.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public Guid Uid { get; set; }
+    }
+}
---
+++ src/Sandbox.API/Sandbox.API/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Sandbox.API.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+    }
+}
